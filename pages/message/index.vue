6ca8523e92c06de5745a9ba77508b749b3d0ee46
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-08-11 19:03:39 +0900

    Add faviconとprofile, messageページを追加

---
+++ pages/message/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <v-container>
+        <h1 class="text-center">現在作成中です</h1>
+        <h4 class="text-center">TwitterのDMで連絡をお願いしますmm</h4>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+</script>
