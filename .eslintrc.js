a73c0a1bbaee404f891e75fe356a1624729150d0
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-08-16 12:19:17 +0900

    Add eslint prettierの追加と設定

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: ['plugin:vue/recommended',
+            'prettier',
+          ],
+  plugins: ['nuxt'],
+  rules: {
+    semi: [2, 'never'],
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-self-closing': 'off' //空タグを許可する
+  }
+}
