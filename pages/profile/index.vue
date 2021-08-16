d0a43c758caf8c6d9ac4ec618223c44fe181bd43
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-08-16 15:28:55 +0900

    Fix testでわざと修正した部分をもとに戻す

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -3,7 +3,7 @@
     <Header />
     <v-main>
       <v-container>
-        <h1 class="text-center">現在作成中です</h1/////>
+        <h1 class="text-center">現在作成中です</h1>
         <h4 class="text-center">
           新宿のベンチャーでwebエンジニアやってます！<br>趣味はサーフィン！
         </h4>
