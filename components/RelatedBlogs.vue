0c87e25f96394e4224a64931f66cc3749beb04b5
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-08-25 19:18:40 +0900

    Fix レイアウトとスタイの調整、ts化

--- components/RelatedBlogs.vue
+++ components/RelatedBlogs.vue
@@ -1,37 +1,42 @@
 <template>
-  <div class="wrapper">
+  <v-container>
     <h2 class="pageTitle">関連記事</h2>
-    <ul class="lists">
-      <li v-for="blog in blogs" :key="blog.id" class="list">
-        <nuxt-link :to="`/${blog.id}`" class="link">
-          <picture>
-            <source
-              type="image/webp"
-              :data-srcset="blog.ogimage.url + '?w=820&fm=webp'"
-            />
-            <img
-              :src="blog.ogimage.url + '?w=820'"
-              class="img lazyload"
-              alt
-            />
-          </picture>
-          <dl class="content">
-            <dt class="title">{{ blog.title }}</dt>
-            <dd>
-              <!-- <Meta
-                :created-at="blog.createdAt"
-                :category="blog.category"
-              /> -->
-            </dd>
-          </dl>
+      <v-card v-for="blog in blogs" :key="blog.id" class="mb-6 hover">
+        <nuxt-link :to="`/${blog.id}`" class="c-p" tag="dev">
+          <v-container>
+            <v-row>
+              <v-col cols="4">
+                <picture>
+                  <source
+                    type="image/webp"
+                    :data-srcset="blog.ogimage.url + '?w=820&fm=webp'"
+                  />
+                  <img
+                    :src="blog.ogimage.url + '?w=820'"
+                    class="img lazyload"
+                    alt
+                  />
+                </picture>
+              </v-col>
+                <v-col cols="8">
+                  <v-card-title>
+                    {{ blog.title }}
+                  </v-card-title>
+                </v-col>
+              </v-row>
+                <!-- <Meta
+                  :created-at="blog.createdAt"
+                  :category="blog.category"
+                /> -->
+          </v-container>
         </nuxt-link>
-      </li>
-    </ul>
-  </div>
+      </v-card>
+  </v-container>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
   props: {
     blogs: {
       type: Array,
@@ -39,10 +44,15 @@ export default {
       default: () => [],
     },
   },
-}
+})
 </script>
 
 <style scoped>
+.hover:hover {
+    transition: .3s;
+    opacity: 0.5;
+  }
+
 @media (min-width: 1160px) {
   .pageTitle {
     font-size: 20px;
@@ -53,38 +63,14 @@ export default {
     border-radius: 5px;
   }
 
-  .lists {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-  }
-
-  .list {
-    width: 340px;
-    border-radius: 5px;
-    transition: box-shadow 0.1s linear;
-
-    &:hover {
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-  }
-
   .img {
     width: 340px;
     height: 178px;
     border-radius: 5px 5px 0 0;
   }
-
-  .content {
-    padding: 10px 10px 0;
-  }
-
-  .title {
-    font-size: 20px;
-    font-weight: bold;
-  }
 }
 @media (min-width: 820px) and (max-width: 1160px) {
+
   .pageTitle {
     font-size: 20px;
     font-weight: bold;
@@ -94,37 +80,13 @@ export default {
     border-radius: 5px;
   }
 
-  .lists {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-  }
-
-  .list {
-    width: 300px;
-    border-radius: 5px;
-    transition: box-shadow 0.1s linear;
-
-    &:hover {
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-  }
-
   .img {
     width: 300px;
     height: 157px;
     border-radius: 5px 5px 0 0;
   }
-
-  .content {
-    padding: 10px 10px 0;
-  }
-
-  .title {
-    font-size: 20px;
-    font-weight: bold;
-  }
 }
+
 @media (max-width: 820px) {
   .pageTitle {
     font-size: 20px;
@@ -135,20 +97,9 @@ export default {
     border-radius: 5px;
   }
 
-  .list {
-    width: 100%;
-    border-radius: 5px;
-  }
-
   .img {
     width: 100%;
     border-radius: 5px;
-  }
-
-  .title {
-    padding-top: 5px;
-    font-size: 18px;
-    font-weight: bold;
   }
 }
 </style>
