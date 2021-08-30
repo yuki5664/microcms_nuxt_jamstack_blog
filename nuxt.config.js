1e7b906552b158b9aa956c2bacc647b1471b90f4
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-08-30 19:15:01 +0900

    Fix 不必要なコメントの削除 close #25

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,10 +23,8 @@ export default {
       })
     },
   },
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'タナカ ユウキのポートレートサイト',
     htmlAttrs: {
@@ -67,24 +65,19 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxt/typescript-build',
     '@nuxtjs/vuetify'
   ],
 
-  // setting typescript typecheck
   typescript: {
     typeCheck: {
       eslint: {
@@ -93,11 +86,9 @@ export default {
     },
   },
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
   ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     postcss: {
       plugins: {
