f9bdceeea9969c706d14474f57d48d8662684063
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-09-08 09:58:27 +0900

    Hotfix fix drawer link

--- components/Header.vue
+++ components/Header.vue
@@ -52,7 +52,7 @@
           <v-list-item
             v-for="link in links"
             :key="link.title"
-            :to="`/` + link.url"
+            :to="`/` + `${link.path}`"
             :nuxt="true"
           >
           <v-icon>
