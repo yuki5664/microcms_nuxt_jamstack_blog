c0af545cfcfcc62fc9b0a1acec139a8073bee06d
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-10-18 17:18:56 +0900

    Add google form to message page

--- pages/message/index.vue
+++ pages/message/index.vue
@@ -3,12 +3,9 @@
     <Header />
     <v-main>
       <v-container>
-        <h1 class="text-center">
-          現在作成中です
-        </h1>
-        <h4 class="text-center">
-          TwitterのDMで連絡をお願いしますmm
-        </h4>
+        <v-row class="my-6">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScCXoSv4g-rY10mglTACR9aA80xhLHiSn5ptgc017i94c5Yxg/viewform?embedded=true" width="100%" height="843" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+	</v-row>
       </v-container>
     </v-main>
   </v-app>
